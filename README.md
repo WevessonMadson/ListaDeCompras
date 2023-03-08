6c5f235951bdfc5806f5f15f3f1c21ab47a49cfc
WevessonMadson <wevesson.madson@hotmail.com>
2023-03-07 21:17:34 -0300

    adicionada toda a lista, o readme, o estilo, o favicon e corrigida a função de cópia

---
+++ README.md
@@ -0,0 +1,21 @@
+# Lista de Compras 
+
+Projeto construído para ajudar com a lista de compras mensal (utilidade doméstica).
+
+[🔗 Clique aqui para acessar](https://wevessonmadson.github.io/ListaDeCompras/)
+
+
+## 🛠 Tecnologias
+
+- HTML
+- CSS
+- JAVASCRIPT
+- Git e Github
+
+## Funcionalidades
+
+- Geração de uma lista de compras personalizada, de acordo com cadastro prévio de quantidades mínimas;
+
+## 💛 Contato
+
+<a href="https://www.linkedin.com/in/wevesson-madson-9a5a4615a/"><img src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white" /></a>
--- js/index.js
+++ js/index.js
@@ -1,15 +1,58 @@
 const produtos = [
-    { descricao: "Feijão Carioca", qtdMin: 3, qtdAtual: 0 },
-    { descricao: "Feijão Macassar", qtdMin: 2, qtdAtual: 0 },
-    { descricao: "Feijão Preto", qtdMin: 1, qtdAtual: 0 },
-    { descricao: "Arroz Parbolizado", qtdMin: 3, qtdAtual: 0 },
-    { descricao: "Arroz Branco", qtdMin: 5, qtdAtual: 0 },
-    { descricao: "Fubá", qtdMin: 15, qtdAtual: 0 },
-    { descricao: "Macarrão", qtdMin: 10, qtdAtual: 0 },
+    { descricao: "Arroz Parbolizado", qtdMin: 3, qtdAtual: 0, unidade: "Kg" },
+    { descricao: "Arroz Branco", qtdMin: 2, qtdAtual: 0, unidade: "Kg" },
+    { descricao: "Feijão Carioca", qtdMin: 3, qtdAtual: 0, unidade: "Kg" },
+    { descricao: "Feijão Macassar", qtdMin: 2, qtdAtual: 0, unidade: "Kg" },
+    { descricao: "Açúcar", qtdMin: 4, qtdAtual: 0, unidade: "Kg" },
+    { descricao: "Farinha", qtdMin: 1, qtdAtual: 0, unidade: "Kg" },
+    { descricao: "Sal", qtdMin: 4, qtdAtual: 0, unidade: "Kg" },
+    { descricao: "Tempero", qtdMin: 4, qtdAtual: 0, unidade: "Pct" },
+    { descricao: "Coloral", qtdMin: 4, qtdAtual: 0, unidade: "Pct" },
+    { descricao: "Molho Shoyo", qtdMin: 2, qtdAtual: 0, unidade: "Und" },
+    { descricao: "Macarrão", qtdMin: 15, qtdAtual: 0, unidade: "Pct" },
+    { descricao: "Fubá", qtdMin: 20, qtdAtual: 0, unidade: "Und" },
+    { descricao: "Ketchup", qtdMin: 1, qtdAtual: 0, unidade: "Und" },
+    { descricao: "Molho de Tomate", qtdMin: 4, qtdAtual: 0, unidade: "Und" },
+    { descricao: "Dueto Milho Verde", qtdMin: 4, qtdAtual: 0, unidade: "Und" },
+    { descricao: "Queijo Ralado", qtdMin: 3, qtdAtual: 0, unidade: "Und" },
+    { descricao: "Milho de Pipoca", qtdMin: 2, qtdAtual: 0, unidade: "Pct" },
+    { descricao: "Leite em Pó", qtdMin: 5, qtdAtual: 0, unidade: "Pct" },
+    { descricao: "Chocolate em Pó", qtdMin: 1, qtdAtual: 0, unidade: "Pct" },
+    { descricao: "Mistura de Morango em Pó", qtdMin: 1, qtdAtual: 0, unidade: "Pct" },
+    { descricao: "Amido de Milho", qtdMin: 1, qtdAtual: 0, unidade: "Cx" },
+    { descricao: "Aveia em Flocos", qtdMin: 2, qtdAtual: 0, unidade: "Cx" },
+    { descricao: "Oléo", qtdMin: 4, qtdAtual: 0, unidade: "Und" },
+    { descricao: "Vinagre", qtdMin: 1, qtdAtual: 0, unidade: "Und" },
+    { descricao: "Leite Condensado", qtdMin: 3, qtdAtual: 0, unidade: "Cx" },
+    { descricao: "Creme de Leite", qtdMin: 4, qtdAtual: 0, unidade: "Cx" },
+    { descricao: "Goibada", qtdMin: 3, qtdAtual: 0, unidade: "Und" },
+    { descricao: "Cream Cracker", qtdMin: 3, qtdAtual: 0, unidade: "Pct" },
+    { descricao: "Coquinho", qtdMin: 2, qtdAtual: 0, unidade: "Pct" },
+    { descricao: "Biscoito Maizena", qtdMin: 2, qtdAtual: 0, unidade: "Pct" },
+    { descricao: "Rosquinha", qtdMin: 2, qtdAtual: 0, unidade: "Pct" },
+    { descricao: "Café", qtdMin: 1, qtdAtual: 0, unidade: "Pct" },
+    { descricao: "Trigo", qtdMin: 1, qtdAtual: 0, unidade: "Kg" },
+    { descricao: "Tempero de Frango", qtdMin: 0.5, qtdAtual: 0, unidade: "Kg" },
+    { descricao: "Guardanapo", qtdMin: 2, qtdAtual: 0, unidade: "Und" },
+    { descricao: "Filme PVC", qtdMin: 1, qtdAtual: 0, unidade: "Und" },
+    { descricao: "Papel Alumínio", qtdMin: 1, qtdAtual: 0, unidade: "Und" },
+    { descricao: "Palito de Dentes", qtdMin: 1, qtdAtual: 0, unidade: "Und" },
+    { descricao: "Herbissimo", qtdMin: 2, qtdAtual: 0, unidade: "Und" },
+    { descricao: "Esponja", qtdMin: 3, qtdAtual: 0, unidade: "Und" },
+    { descricao: "Esponja de Aço", qtdMin: 3, qtdAtual: 0, unidade: "Und" },
+    { descricao: "Papel Higiênico", qtdMin: 12, qtdAtual: 0, unidade: "Und" },
+    { descricao: "Creme Dental", qtdMin: 6, qtdAtual: 0, unidade: "Und" },
+    { descricao: "Sabonete", qtdMin: 12, qtdAtual: 0, unidade: "Und" },
+    { descricao: "Sabão em Barra", qtdMin: 5, qtdAtual: 0, unidade: "Und" },
+    { descricao: "Sabão em Pó", qtdMin: 5, qtdAtual: 0, unidade: "Pct" },
+    { descricao: "Água Sanitária", qtdMin: 5, qtdAtual: 0, unidade: "Und" },
+    { descricao: "Veja", qtdMin: 3, qtdAtual: 0, unidade: "Und" },
+    { descricao: "Amaciante", qtdMin: 1, qtdAtual: 0, unidade: "Und" },
+    { descricao: "Desinfetante", qtdMin: 1, qtdAtual: 0, unidade: "Und" },
 ];
 
-function createTableBody(descricao, qtdAtual) {
-    return `<tr class='trTableBody'><td class='descProdTable'>${descricao}</td><td class='qtdProdTable'><input onfocus="selectContent()" oninput='updateData()' class='inputValue' type='number' value='${qtdAtual}'></td></tr>`;
+function createTableBody(descricao, qtdAtual, unidade) {
+    return `<tr class='trTableBody'><td class='descProdTable'>${descricao}</td><td class='qtdProdTable'><input onfocus="selectContent()" oninput='updateData()' class='inputValue' type='number' value='${qtdAtual}'><span class='unidade'>${unidade}</span</td></tr>`;
 };
 
 function createTable(dataTables) {
@@ -41,7 +84,7 @@ function renderReturn(e) {
     let dataTable = "";
 
     objectProdutos.forEach(produto => {
-        dataTable += createTableBody(produto.descricao, produto.qtdAtual);
+        dataTable += createTableBody(produto.descricao, produto.qtdAtual, produto.unidade);
     });
 
     table.innerHTML = createTable(dataTable);
@@ -63,7 +106,7 @@ function selectContent() {
 }
 
 async function gerarLista() {    
-    const dataLista = JSON.parse(localStorage.getItem("lastList"));
+    const dataLista = JSON.parse(localStorage.getItem("lastList")) || produtos;
 
     let lista = "";
 
@@ -74,7 +117,7 @@ async function gerarLista() {
         }
     });
 
-    await window.navigator.clipboard.writeText(lista);
+    await navigator.clipboard.writeText(lista);
 
     alert("Copiado para a area de transferencia");
 };
